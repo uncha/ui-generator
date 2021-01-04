0627f39b991c56b7bbb6298f6fef9030de817088
kyutaekim <uncha@ex-it.co.kr>
2021-01-04 10:02:07 +0900

    merge

--- src/renderer/components/preview/form.vue
+++ src/renderer/components/preview/form.vue
@@ -1,43 +1,14 @@
-<<<<<<< HEAD
-
-        <template>
-          <b-form @submit.prevent="onSubmit">
-            <b-form-group :label="'제목'" :label-cols="4">
-                <b-form-input
-                  type="text"
-                  maxlength="50"
-                  :placeholder="'제목을 입력해 주세요.'"
-                  v-model="$v.form.ttl.$model"
-                  :state="validateState('ttl')"
-                />
-                <b-form-invalid-feedback>
-                  입력해주세요.
-                </b-form-invalid-feedback>
-        
-          </b-form-group>
-        <b-form-group :label="'시작일'" :label-cols="4">
-        <b-form-datepicker v-model="schBeginDt"></b-form-datepicker>
-      
-          </b-form-group>
-        <b-form-group :label="'종료일'" :label-cols="4">
-        <b-form-datepicker v-model="schEndDt"></b-form-datepicker>
-      
-          </b-form-group>
-        
-            <button type="submit" class="btn btn-primary">전송</button>
-          </b-form>
-=======
 <template>
   <div>
-    <b-form @submit.prevent="onSubmit" v-if="form != ''">
-      <b-form-group :label="item.label" :label-cols="baseForm.config.labelCols" v-for="(item,i) in baseForm.rows" :key="`row-${i}`">
+    <b-form v-if="form != ''" @submit.prevent="onSubmit">
+      <b-form-group v-for="(item,i) in baseForm.rows" :key="`row-${i}`" :label="item.label" :label-cols="baseForm.config.labelCols">
         <template v-if="item.type == 'input'">
           <template v-if="item.requiredOptions">
             <b-form-input
+              v-model="$v.form[item.key].$model"
               type="text"
               :maxlength="item.maxLength"
               :placeholder="item.placeholder"
-              v-model="$v.form[item.key].$model"
               :state="validateState(item.key)"
             />
             <b-form-invalid-feedback>
@@ -46,10 +17,10 @@
           </template>
           <template v-else>
             <b-form-input
+              v-model="$v.form[item.key].$model"
               type="text"
               :maxlength="item.maxLength"
               :placeholder="item.placeholder"
-              v-model="$v.form[item.key].$model"
             />
           </template>
         </template>
@@ -58,8 +29,7 @@
             v-model="form[item.key]"
             :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
             locale="kr"
-          ></b-form-datepicker>
->>>>>>> 86ba8af9ff6cf2b037774867ae0f39d5ce5a4326
+          />
         </template>
       </b-form-group>
       <button type="submit" class="btn btn-primary">전송</button>
@@ -73,8 +43,8 @@ import { required, minLength, alphaNum } from 'vuelidate/lib/validators'
 
 export default {
   mixins: [validationMixin],
-  validations() {
-    if(this.formValidations != '') {
+  validations () {
+    if (this.formValidations != '') {
       return {
         form: this.formValidations
       }
@@ -82,44 +52,33 @@ export default {
   },
   props: {
     baseForm: {
-      type:Object,
-      required:true
+      type: Object,
+      required: true
     }
   },
   data () {
     return {
-      form:'',
-      formValidations:''
+      form: '',
+      formValidations: ''
     }
   },
   created () {
-    let form = {}
-    let formValidations = {
+    const form = {}
+    const formValidations = {
     }
 
     _.forEach(this.baseForm.rows, (item, i) => {
       form[item.key] = eval(item.value)
-      if(item.requiredOptions) {
+      if (item.requiredOptions) {
         formValidations[item.key] = { required }
       }
-    });
+    })
 
     this.formValidations = formValidations
     this.form = form
   },
   mounted () {
 
-<<<<<<< HEAD
-          export default {
-	          mixins: [validationMixin],
-            validations: {
-          		form: {ttl:{required},schBeginDt:{required},schEndDt:{required},}
-            },
-            data () {
-              return "\n        {\n          form: {\n      ttl:schBeginDt:moment().format('YYYY-MM-DD')schEndDt:moment().format('YYYY-MM-DD')\n          }\n        }\n      "
-            },
-            mounted () {
-=======
   },
   methods: {
   	validateState (name) {
@@ -129,7 +88,6 @@ export default {
   	onSubmit () {
   		this.$v.form.$touch()
   		if (this.$v.form.$anyError) {
->>>>>>> 86ba8af9ff6cf2b037774867ae0f39d5ce5a4326
 
   		}
   	}
